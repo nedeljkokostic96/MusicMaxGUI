766e8dc4f215d18deda34bd23bee95ca258b8f40
DNovakovic101 <dusannovakovic32@gmail.com>
2021-05-25 21:19:51 +0200

    @T4004:R Added commenting on forum themes and opening new ones

--- src/app/components/home/forum-item/forum-item.component.ts
+++ src/app/components/home/forum-item/forum-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CommentDialogComponent } from '../../dialogs/comment-dialog/comment-dialog.component';
 export interface Forum{
   title:string;
 }
@@ -9,9 +11,15 @@ export interface Forum{
 })
 export class ForumItemComponent implements OnInit {
   @Input() forum:Forum;
-  constructor() { }
+  constructor(public dialog:MatDialog) { }
 
   ngOnInit(): void {
   }
+  openDialogAddForum(){
+    const dialogRef = this.dialog.open(CommentDialogComponent, {
+     
+      data: {textt:'forum'}
+    });
 
+  }
 }
